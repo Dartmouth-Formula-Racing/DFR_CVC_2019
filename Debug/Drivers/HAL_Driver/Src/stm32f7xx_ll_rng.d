9b52ac4df01d006b49bbab63150aa814230d696e
leina05 <leina05@gmail.com>
2018-12-04 15:30:23 -0500

    cleaned up files to be ignored by deletiing remote Debug directory

--- Debug/Drivers/HAL_Driver/Src/stm32f7xx_ll_rng.d
+++
@@ -1,2 +0,0 @@
-Drivers/HAL_Driver/Src/stm32f7xx_ll_rng.o: \
- ../Drivers/HAL_Driver/Src/stm32f7xx_ll_rng.c
--- Debug/Drivers/HAL_Driver/Src/stm32f7xx_ll_usart.d
+++
@@ -1,2 +0,0 @@
-Drivers/HAL_Driver/Src/stm32f7xx_ll_usart.o: \
- ../Drivers/HAL_Driver/Src/stm32f7xx_ll_usart.c
