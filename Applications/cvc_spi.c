447554ae776506fce04616aa44bdc81d4f8ebc49
Alexander S. Newman <f002ccx@dartmouth.edu>
2019-01-30 17:37:29 -0500

    Works with outputs set in task, but still need protection for inputs/outputs vectors!

--- Applications/cvc_spi.c
+++ Applications/cvc_spi.c
@@ -157,12 +157,7 @@ void SPI_PLC_Set_Inputs(void)
 	SPI_inputs_vector.BMS_safety_circuit_fault 		= CLT_Read.bit.IN6;
 	SPI_inputs_vector.Bamocar_safety_circuit_fault 	= CLT_Read.bit.IN7;
 
-	SPI_outputs_vector.safety = SPI_inputs_vector.ICE_enable;
-	SPI_outputs_vector.ready_to_drive = SPI_inputs_vector.Motor_enable;
-	SPI_outputs_vector.rfg = SPI_inputs_vector.Ready_to_drive;
-	SPI_outputs_vector.ignition_kill = SPI_inputs_vector.Dash_BRB_press;
-	SPI_outputs_vector.downshift_solenoid = SPI_inputs_vector.IMD_safety_circuit_fault;
-	SPI_outputs_vector.upshift_solenoid = SPI_inputs_vector.BMS_safety_circuit_fault;
+
 
 }
 
--- Applications/synchronous.c
+++ Applications/synchronous.c
@@ -13,7 +13,7 @@
 #include "cvc_spi.h"
 
 /* Volatile variables --------------------------------------------------------*/
-
+extern volatile SPI_outputs_vector_t SPI_outputs_vector;
 
 /**
   * @brief Fast synchronous task (100 Hz)
@@ -24,6 +24,12 @@ void _10_ms_Task(void * parameters)
 	{
 		vTaskDelay((TickType_t) 10/portTICK_PERIOD_MS);
 
+		SPI_outputs_vector.safety = SPI_inputs_vector.ICE_enable;
+		SPI_outputs_vector.ready_to_drive = SPI_inputs_vector.Motor_enable;
+		SPI_outputs_vector.rfg = SPI_inputs_vector.Ready_to_drive;
+		SPI_outputs_vector.ignition_kill = SPI_inputs_vector.Dash_BRB_press;
+		SPI_outputs_vector.downshift_solenoid = SPI_inputs_vector.IMD_safety_circuit_fault;
+		SPI_outputs_vector.upshift_solenoid = SPI_inputs_vector.BMS_safety_circuit_fault;
 
 		initiate_SPI_transmission();
 	}
