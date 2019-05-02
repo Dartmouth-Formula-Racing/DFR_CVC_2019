9396624825313f8c085b7eaa6e3c73d4efd5a542
Alexander S. Newman <f002ccx@dartmouth.edu>
2019-05-02 09:21:07 -0400

    endurance mode + updated TPS

--- Applications/inc/torque_command.h
+++ Applications/inc/torque_command.h
@@ -13,10 +13,14 @@
 #include "bamocar.h"
 
 #define OFFSET_MAX (16384.0f)
-#define LOWER_TPS_LIMIT (3.0f)
-#define UPPER_TPS_LIMIT (232.0f)
+#define LOWER_TPS_LIMIT (16.0f)
+#define UPPER_TPS_LIMIT (202.0f)
 #define MULTIPLIER (1.0f)
 
+#define MIN_BATTERY_VOLTAGE (230.0f)
+#define MAX_BATTERY_VOLTAGE (300.0f)
+
+#define RPM_DEAD_COUNT 100
 
 void torque_command(void);
 
--- Applications/torque_command.c
+++ Applications/torque_command.c
@@ -12,7 +12,13 @@
 uint8_t ICE_tps = 0;
 float calc_torque = 0.0f;
 uint16_t torque_cmd = 0;
+
+uint8_t battery_voltage = 0;
+uint8_t engine_rpm = 0;
+
 float tps_percentage = 0.0f;
+float decay_val = 0.0f;
+int count = 0;
 
 void torque_command(void)
 {
@@ -29,7 +35,27 @@ void torque_command(void)
 		tps_percentage = 0;
 	}
 
-	calc_torque = tps_percentage*OFFSET_MAX*MULTIPLIER;
+	battery_voltage = CAN_inputs[BATT_VOLTAGE]/100;
+	decay_val = (battery_voltage-MIN_BATTERY_VOLTAGE)/(MAX_BATTERY_VOLTAGE-MIN_BATTERY_VOLTAGE);
+
+
+	engine_rpm = CAN_inputs[ENG_RPM];
+
+	if (engine_rpm == 0)
+	{
+		count++;
+
+		if (count > RPM_DEAD_COUNT)
+		{
+			calc_torque = tps_percentage*OFFSET_MAX*MULTIPLIER;
+		}
+	}
+	else
+	{
+		count = 0;
+		calc_torque = decay_val*tps_percentage*OFFSET_MAX*MULTIPLIER;
+	}
+
 
 	torque_cmd = calc_torque;
 
