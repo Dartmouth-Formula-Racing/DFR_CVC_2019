a2970ac9d14840711206341d3249b1bd9190928e
bennewhall <bennewhall@me.com>
2019-11-17 21:23:36 -0500

    started simplifying tasks in synchronous

--- Applications/synchronous.c
+++ Applications/synchronous.c
@@ -19,22 +19,7 @@ void _10_ms_Task(void * parameters)
 	{
 		vTaskDelayUntil(&LastWakeTime, (TickType_t) 10/portTICK_PERIOD_MS);
 
-#if LOGGING_TEST == 0
-		TickType_t start = xTaskGetTickCount();
-		state_machine();
 
-		if (cvc_state == DRIVE)
-		{
-			torque_command();
-			safety_monitor();
-		}
-
-		TickType_t end = xTaskGetTickCount();
-
-		if(end > start) {
-			int good = 1;
-		}
-#endif
 
 	}
 }
@@ -53,10 +38,7 @@ void _20_ms_Task(void * parameters)
 	{
 		vTaskDelayUntil(&LastWakeTime, (TickType_t) 20/portTICK_PERIOD_MS);
 
-		if (cvc_state > VOLTAGE_CHECK)
-		{
-			log_data();
-		}
+
 	}
 
 }
