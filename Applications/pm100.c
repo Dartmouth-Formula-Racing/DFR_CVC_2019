ccc9e6febe7e93e1d57ae9775d9d904f6bc3d84a
bennewhall <bennewhall@me.com>
2020-02-25 14:19:13 -0500

    bug fixes

--- Applications/pm100.c
+++ Applications/pm100.c
@@ -122,15 +122,15 @@ void parameter_read_command_2(uint16_t parameter_address)
  */
 void command_msg_1(uint16_t torque_command, uint16_t speed_command, uint8_t direction_command, uint8_t inverter_enable, uint8_t inverter_discharge, uint8_t speed_mode_enable, uint16_t commanded_torque_limit){
 	pm100_command_msg_1.data._8[0] = (torque_command & 0x00FF);
-	pm100_command_msg_1.data._8[1] = (torque_command & 0xFF00 >> 8);
+	pm100_command_msg_1.data._8[1] = ((torque_command & 0xFF00) >> 8);
 	pm100_command_msg_1.data._8[2] = (speed_command & 0x00FF);
-	pm100_command_msg_1.data._8[3] = (speed_command & 0xFF00 >> 8);
+	pm100_command_msg_1.data._8[3] = ((speed_command & 0xFF00) >> 8);
 	pm100_command_msg_1.data._8[4] = direction_command;
 	pm100_command_msg_1.data._8[5] = inverter_enable;
 	pm100_command_msg_1.data._8[5] |= inverter_discharge << 1;
 	pm100_command_msg_1.data._8[5] |= speed_mode_enable << 2;
 	pm100_command_msg_1.data._8[6] = (commanded_torque_limit & 0x00FF);
-	pm100_command_msg_1.data._8[7] = (commanded_torque_limit & 0xFF00 >> 8);
+	pm100_command_msg_1.data._8[7] = ((commanded_torque_limit & 0xFF00) >> 8);
 
 	CAN_Send(pm100_command_msg_1);
 }
@@ -148,15 +148,15 @@ void command_msg_1(uint16_t torque_command, uint16_t speed_command, uint8_t dire
  */
 void command_msg_2(uint16_t torque_command, uint16_t speed_command, uint8_t direction_command, uint8_t inverter_enable, uint8_t inverter_discharge, uint8_t speed_mode_enable, uint16_t commanded_torque_limit){
 	pm100_command_msg_2.data._8[0] = (torque_command & 0x00FF);
-	pm100_command_msg_2.data._8[1] = (torque_command & 0xFF00 >> 8);
+	pm100_command_msg_2.data._8[1] = ((torque_command & 0xFF00) >> 8);
 	pm100_command_msg_2.data._8[2] = (speed_command & 0x00FF);
-	pm100_command_msg_2.data._8[3] = (speed_command & 0xFF00 >> 8);
+	pm100_command_msg_2.data._8[3] = ((speed_command & 0xFF00) >> 8);
 	pm100_command_msg_2.data._8[4] = direction_command;
 	pm100_command_msg_2.data._8[5] = inverter_enable;
 	pm100_command_msg_2.data._8[5] |= inverter_discharge << 1;
 	pm100_command_msg_2.data._8[5] |= speed_mode_enable << 2;
 	pm100_command_msg_2.data._8[6] = (commanded_torque_limit & 0x00FF);
-	pm100_command_msg_2.data._8[7] = (commanded_torque_limit & 0xFF00 >> 8);
+	pm100_command_msg_2.data._8[7] = ((commanded_torque_limit & 0xFF00) >> 8);
 
 	CAN_Send(pm100_command_msg_2);
 }
