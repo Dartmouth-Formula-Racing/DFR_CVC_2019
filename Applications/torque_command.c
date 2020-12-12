3cd20f278cff85aebdf03d361508dc9f8e8d1e46
Vico Lee <vicoleezy@gmail.com>
2020-12-11 20:05:24 -0500

    added linear acceleration mode to torque_command

--- Applications/torque_command.c
+++ Applications/torque_command.c
@@ -5,7 +5,6 @@
  *      Author: f002ccx
  */
 
-
 #include "torque_command.h"
 
 
@@ -14,7 +13,51 @@ float calc_torque = 0.0f;
 uint16_t torque_cmd = 0;
 float tps_percentage = 0.0f;
 
-void torque_command(void)
+/** 
+ * @brief a way to switch to different drive modes
+ * @param mode the mode at which two characters are taken as inputs and sends to car
+*/
+void torque_command(char mode[2])
 {
+    if(HAL_GPIO_ReadPin(B1_GPIO_PORT, B1_PIN)&& CAN_inputs[INVERTER_ENABLE_LOCKOUT] == 0)
+    {
+		switch (mode)
+        {
+            case 'LA':                   // linear acceleration mode
+                calc_torque = linear_acceleration();
+                break;
+                
+            default:
+                break;
+        }
+    } else {
+        calc_torque = 0;
+    }
 
+    command_msg_1(calc_torque, 0, 1, 1, 0, 0, 0);       // send to rinehart 1
+    command_msg_2(calc_torque, 0, 1, 1, 0, 0, 0);       // send to rinehart 2
+
+}
+
+/**
+ * @brief the way to calculate torque for the car acceleration in a straight line
+ */
+double linear_acceleration(void)
+{
+    tps = CAN_inputs[ENG_TPS];
+
+	tps_percentage = ((tps - LOWER_TPS_LIMIT)/(UPPER_TPS_LIMIT - LOWER_TPS_LIMIT) * 100) ** 2;      // can test with other growing functions e.g e^x
+
+	if (tps_percentage >= 100)
+	{
+		tps_percentage = 100;
+	}
+	else if (tps_percentage <= 0)
+	{
+		tps_percentage = 0;
+	}
+
+	calc_torque = (tps_percentage * OFFSET_MAX * MULTIPLIER) / 100;
+    
+    return calc_torque;
 }
