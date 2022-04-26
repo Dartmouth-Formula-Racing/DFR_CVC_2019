419d3d5bb3e8afd3cbc59ddeb6877dcf1b7e98b4
andreigerashchenko <67013056+andreigerashchenko@users.noreply.github.com>
2022-04-25 23:55:46 -0400

    Fixed debugging setup
    No more pointless breakpoint on CPU_CACHE_Enable();

--- Debug/Middlewares/FatFs/src/ff.d
+++ Debug/Middlewares/FatFs/src/ff.d
@@ -1,23 +1,23 @@
 Middlewares/FatFs/src/ff.o: ../Middlewares/FatFs/src/ff.c \
  ../Middlewares/FatFs/src/ff.h ../Middlewares/FatFs/src/integer.h \
  ../Middlewares/FatFs/src/ffconf.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/FreeRTOS.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Applications/inc/FreeRTOSConfig.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/projdefs.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/portable.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/deprecated_definitions.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/portable/GCC/ARM_CM7/portmacro.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/mpu_wrappers.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/task.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/list.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/timers.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/task.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/queue.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/semphr.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/queue.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/event_groups.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/timers.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/FreeRTOS.h \
+ O:/DFR/DFR_CVC/Applications/inc/FreeRTOSConfig.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/projdefs.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/portable.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/deprecated_definitions.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/portable/GCC/ARM_CM7/portmacro.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/mpu_wrappers.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/task.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/list.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/timers.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/task.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/queue.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/semphr.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/queue.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/event_groups.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/timers.h \
  ../Middlewares/FatFs/src/diskio.h
 
 ../Middlewares/FatFs/src/ff.h:
@@ -26,38 +26,38 @@ Middlewares/FatFs/src/ff.o: ../Middlewares/FatFs/src/ff.c \
 
 ../Middlewares/FatFs/src/ffconf.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/FreeRTOS.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/FreeRTOS.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Applications/inc/FreeRTOSConfig.h:
+O:/DFR/DFR_CVC/Applications/inc/FreeRTOSConfig.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/projdefs.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/projdefs.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/portable.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/portable.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/deprecated_definitions.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/deprecated_definitions.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/portable/GCC/ARM_CM7/portmacro.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/portable/GCC/ARM_CM7/portmacro.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/mpu_wrappers.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/mpu_wrappers.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/task.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/task.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/list.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/list.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/timers.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/timers.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/task.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/task.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/queue.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/queue.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/semphr.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/semphr.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/queue.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/queue.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/event_groups.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/event_groups.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/timers.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/timers.h:
 
 ../Middlewares/FatFs/src/diskio.h:
--- Debug/Middlewares/FreeRTOS/Source/croutine.d
+++ Debug/Middlewares/FreeRTOS/Source/croutine.d
@@ -1,32 +1,32 @@
 Middlewares/FreeRTOS/Source/croutine.o: \
  ../Middlewares/FreeRTOS/Source/croutine.c \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/FreeRTOS.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Applications/inc/FreeRTOSConfig.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/projdefs.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/portable.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/deprecated_definitions.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/portable/GCC/ARM_CM7/portmacro.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/mpu_wrappers.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/task.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/list.h \
- C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/croutine.h
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/FreeRTOS.h \
+ O:/DFR/DFR_CVC/Applications/inc/FreeRTOSConfig.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/projdefs.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/portable.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/deprecated_definitions.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/portable/GCC/ARM_CM7/portmacro.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/mpu_wrappers.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/task.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/list.h \
+ O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/croutine.h
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/FreeRTOS.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/FreeRTOS.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Applications/inc/FreeRTOSConfig.h:
+O:/DFR/DFR_CVC/Applications/inc/FreeRTOSConfig.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/projdefs.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/projdefs.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/portable.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/portable.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/deprecated_definitions.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/deprecated_definitions.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/portable/GCC/ARM_CM7/portmacro.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/portable/GCC/ARM_CM7/portmacro.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/mpu_wrappers.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/mpu_wrappers.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/task.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/task.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/list.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/list.h:
 
-C:/Users/bridg/Documents/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/croutine.h:
+O:/DFR/DFR_CVC/Middlewares/FreeRTOS/Source/include/croutine.h:
