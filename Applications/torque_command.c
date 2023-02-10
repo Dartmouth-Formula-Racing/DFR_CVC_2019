e329525b316971a7d0b94a56633d50f81835a08e
Henry Scheible <henry.scheible@gmail.com>
2023-02-09 19:27:43 -0500

    add hardcoded sequence

--- Applications/torque_command.c
+++ Applications/torque_command.c
@@ -20,35 +20,127 @@ uint16_t torque_cmd_2 = 0;
 float tps_percentage = 0.0f;
 int errcount = 0;
 
+queue_msg_t pm100_command_msg_1_aa =
+{
+	.Tx_header = {0x0AA, 0, STD, RTR, 0x8},
+	.data._8 = {0, 0, 0, 0, 0, 0, 0, 0}
+};
+
+queue_msg_t pm100_command_msg_1_c0 =
+{
+	.Tx_header = {0x0C0, 0, STD, RTR, 0x8},
+	.data._8 = {0, 0, 0, 0, 0, 0, 0, 0}
+};
+
+
+
+int count = 0;
+
+
 void torque_command()
 {
-	if (get_cvc_state() == DRIVE) {
-		// Read throttle pots
-		process_throttle();
-		// Convert torque command value
+		if (get_cvc_state()== DRIVE) {
 
-
-		// TODO: get drive state only once for efficiency?
-		if (get_drive_state() == DRIVE) {
-			torque_linear(TORQUE_MULTIPLIER_FORWARD);
-			pm100_torque_command_1(torque_int(calc_torque_1), 0);
-			pm100_torque_command_2(torque_int(calc_torque_2), 1);
-		} else if (get_drive_state() == NEUTRAL) {
-			command_msg_1(0,0,0,0,0,0,0);
-			command_msg_2(0,0,0,0,0,0,0);
-		} else if (get_drive_state() == REVERSE) {
-			torque_linear(TORQUE_MULTIPLIER_REVERSE);
-			pm100_torque_command_1(torque_int(calc_torque_1), 1);
-			pm100_torque_command_2(torque_int(calc_torque_2), 0);
-		} else {
-			command_msg_1(0,0,0,0,0,0,0);
-			command_msg_2(0,0,0,0,0,0,0);
-		}
+	if (count < 1000) {
+		pm100_command_msg_1_aa.data._8[0] = 4;
+		pm100_command_msg_1_aa.data._8[1] = 0;
+		pm100_command_msg_1_aa.data._8[2] = 9;
+		pm100_command_msg_1_aa.data._8[3] = 0;
+		pm100_command_msg_1_aa.data._8[4] = 0;
+		pm100_command_msg_1_aa.data._8[5] = 0;
+		pm100_command_msg_1_aa.data._8[6] = 128;
+		pm100_command_msg_1_aa.data._8[7] = 0;
+		CAN_Send(pm100_command_msg_1_aa);
+	} else if (count < 1500) {
+		pm100_command_msg_1_c0.data._8[0] = 0;
+		pm100_command_msg_1_c0.data._8[1] = 0;
+		pm100_command_msg_1_c0.data._8[2] = 0;
+		pm100_command_msg_1_c0.data._8[3] = 0;
+		pm100_command_msg_1_c0.data._8[4] = 0;
+		pm100_command_msg_1_c0.data._8[5] = 0;
+		pm100_command_msg_1_c0.data._8[6] = 0;
+		pm100_command_msg_1_c0.data._8[7] = 0;
+		CAN_Send(pm100_command_msg_1_c0);
+	} else if (count < 2000) {
+		pm100_command_msg_1_c0.data._8[0] = 40;
+		pm100_command_msg_1_c0.data._8[1] = 0;
+		pm100_command_msg_1_c0.data._8[2] = 0;
+		pm100_command_msg_1_c0.data._8[3] = 0;
+		pm100_command_msg_1_c0.data._8[4] = 1;
+		pm100_command_msg_1_c0.data._8[5] = 1;
+		pm100_command_msg_1_c0.data._8[6] = 0;
+		pm100_command_msg_1_c0.data._8[7] = 0;
+		CAN_Send(pm100_command_msg_1_c0);
+	} else if (count < 2500) {
+		pm100_command_msg_1_c0.data._8[0] = 0;
+		pm100_command_msg_1_c0.data._8[1] = 0;
+		pm100_command_msg_1_c0.data._8[2] = 0;
+		pm100_command_msg_1_c0.data._8[3] = 0;
+		pm100_command_msg_1_c0.data._8[4] = 1;
+		pm100_command_msg_1_c0.data._8[5] = 1;
+		pm100_command_msg_1_c0.data._8[6] = 0;
+		pm100_command_msg_1_c0.data._8[7] = 0;
+		CAN_Send(pm100_command_msg_1_c0);
+	} else if (count < 3000) {
+		pm100_command_msg_1_c0.data._8[0] = 40;
+		pm100_command_msg_1_c0.data._8[1] = 0;
+		pm100_command_msg_1_c0.data._8[2] = 0;
+		pm100_command_msg_1_c0.data._8[3] = 0;
+		pm100_command_msg_1_c0.data._8[4] = 1;
+		pm100_command_msg_1_c0.data._8[5] = 1;
+		pm100_command_msg_1_c0.data._8[6] = 0;
+		pm100_command_msg_1_c0.data._8[7] = 0;
+		CAN_Send(pm100_command_msg_1_c0);
+	} else if (count < 3500) {
+		pm100_command_msg_1_c0.data._8[0] = 100;
+		pm100_command_msg_1_c0.data._8[1] = 255;
+		pm100_command_msg_1_c0.data._8[2] = 0;
+		pm100_command_msg_1_c0.data._8[3] = 0;
+		pm100_command_msg_1_c0.data._8[4] = 1;
+		pm100_command_msg_1_c0.data._8[5] = 0;
+		pm100_command_msg_1_c0.data._8[6] = 0;
+		pm100_command_msg_1_c0.data._8[7] = 0;
+		CAN_Send(pm100_command_msg_1_c0);
+	} else if (count < 4000) {
+		pm100_command_msg_1_c0.data._8[0] = 0;
+		pm100_command_msg_1_c0.data._8[1] = 0;
+		pm100_command_msg_1_c0.data._8[2] = 0;
+		pm100_command_msg_1_c0.data._8[3] = 0;
+		pm100_command_msg_1_c0.data._8[4] = 0;
+		pm100_command_msg_1_c0.data._8[5] = 1;
+		pm100_command_msg_1_c0.data._8[6] = 0;
+		pm100_command_msg_1_c0.data._8[7] = 0;
+		CAN_Send(pm100_command_msg_1_c0);
 	}
-	else {
-		command_msg_1(0,0,0,0,0,0,0);
-		command_msg_2(0,0,0,0,0,0,0);
-	}
+	count = count + 1; }
+//	if (get_cvc_state()== DRIVE) {
+//		// Read throttle pots
+//		process_throttle();
+//		// Convert torque c`ommand value
+//
+//
+////		 TODO: get drive state only once for efficiency?
+//		if (get_drive_state() == DRIVE) {
+//			torque_linear(TORQUE_MULTIPLIER_FORWARD);
+//			pm100_torque_command_1(torque_int(calc_torque_1), 0);
+//			pm100_torque_command_2(torque_int(calc_torque_2), 1);
+//
+//		} else if (get_drive_state() == NEUTRAL) {
+//			command_msg_1(0,0,0,0,0,0,0);
+//			command_msg_2(0,0,0,0,0,0,0);
+//		} else if (get_drive_state() == REVERSE) {
+//			torque_linear(TORQUE_MULTIPLIER_REVERSE);
+//			pm100_torque_command_1(torque_int(calc_torque_1), 1);
+//			pm100_torque_command_2(torque_int(calc_torque_2), 0);
+//		} else {
+//			command_msg_1(0,0,0,0,0,0,0);
+//			command_msg_2(0,0,0,0,0,0,0);
+//		}
+//	}
+//	else {
+//		command_msg_1(0,0,0,0,0,0,0);
+//		command_msg_2(0,0,0,0,0,0,0);
+//	}
 }
 
 int32_t intmap(int32_t x, int32_t in_min, int32_t in_max, int32_t out_min, int32_t out_max)
@@ -99,13 +191,13 @@ uint32_t clamp_throttle(uint32_t d, uint32_t min, uint32_t max) {
 uint16_t torque_int(float torque)
 {
 	int16_t signed_torque = (int16_t)(torque * 10.0f);
-	return (uint16_t)signed_torque;
+	return signed_torque;
 }
 
 void torque_linear(float multiplier)
 {
-	calc_torque_1 = (tps_percentage - 0.25) * UPPER_TORQUE_LIMIT * multiplier;
-	calc_torque_2 = (tps_percentage - 0.25) * UPPER_TORQUE_LIMIT * multiplier;
+	calc_torque_1 = (tps_percentage-0.5) * UPPER_TORQUE_LIMIT * multiplier;
+	calc_torque_2 = (tps_percentage-0.5) * UPPER_TORQUE_LIMIT * multiplier;
 }
 
 //int16_t xBuffer = (CAN_inputs[X_AXIS_ACCELERATION]);
